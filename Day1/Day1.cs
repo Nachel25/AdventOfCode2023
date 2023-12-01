53f4a9752e885c548fe81395e80dc86287b6d0b4
Nachel25 <langjan_nagel@yahoo.de>
2023-12-01 16:02:27 +0100

    Projektdateien hinzufügen.

---
+++ Day1/Day1.cs
@@ -0,0 +1,60 @@
+﻿namespace Day1
+{
+    public class Day1
+    {
+        private readonly string[] _linesArray;
+        private readonly int[] _elfCaloriesArray;
+
+        private int _totalCaloriesTopOne;
+        public int TotalCaloriesTopOne
+        {
+            get
+            {
+                _totalCaloriesTopOne = (from elves in _elfCaloriesArray
+                                        orderby elves descending
+                                        select elves).First();
+                return _totalCaloriesTopOne;
+            }
+        }
+
+        private int _totalCaloriesTopThree;
+        public int TotalCaloriesTopThree
+        {
+            get
+            {
+                _totalCaloriesTopThree = (from elves in _elfCaloriesArray
+                                         orderby elves descending
+                                         select elves).Take(3).Sum();
+                return _totalCaloriesTopThree;
+            }
+        }
+
+        public Day1(string filepath)
+        {
+            _linesArray = File.ReadAllLines(filepath);
+
+            var numberOfElves = 0;
+            foreach (var line in _linesArray)
+            {
+                if (string.IsNullOrEmpty(line))
+                {
+                    numberOfElves++;
+                }
+            }
+            _elfCaloriesArray = new int[numberOfElves + 1];
+
+            var elfIndex = 0;
+            foreach (var line in _linesArray)
+            {
+                if (!string.IsNullOrEmpty(line))
+                {
+                    _elfCaloriesArray[elfIndex] += int.Parse(line);
+                }
+                else
+                {
+                    elfIndex++;
+                }
+            }
+        }
+    }
+}
---
+++ Day1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Call Class Day1
+            var day1 = new Day1(Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\inputFiles\inputDay01.txt"));
+
+            // Part 1
+            Console.WriteLine("\nBeginning Day 1 Part 1");
+            Console.WriteLine("----------------------------------------------------------------");
+            Console.WriteLine($"Elf is carrying {day1.TotalCaloriesTopOne} calories.");
+
+            // Part 2
+            Console.WriteLine("\nBeginning Day 1 Part 2");
+            Console.WriteLine("----------------------------------------------------------------");
+            Console.WriteLine($"The best three elves are carrying {day1.TotalCaloriesTopThree} calories.");
+        }
+    }
+}
